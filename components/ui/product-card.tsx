e12d05d45349f8a9de6436599cdd650039344fdd
Arj-22 <99590153+Arj-22@users.noreply.github.com>
2023-09-18 18:09:44 +0100

    Individual Product Screen

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard { 
     data: Product;
@@ -12,8 +13,14 @@ interface ProductCard {
 
 
 const ProductCard: React.FC<ProductCard> = ({data}) => {
+  const router = useRouter();
+  const handleClick = () => {
+    // In Future change this to data?.name
+    // This could help with SEO as the product name is in the url slug
+    router.push(`/product/${data?.id}`);
+  }
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Images and Actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image alt="Image" src={data?.images?.[0]?.url} fill className="aspect-square object-cover rounded-md"/>
